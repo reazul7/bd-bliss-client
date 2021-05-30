dfcab5bdd510627772d1d8de021de2e4235491db
anikaa137 <anikaa137@gmail.com>
2021-05-30 15:21:18 +0600

    card resize

--- src/components/ProductComponent/ProductComponent.js
+++ src/components/ProductComponent/ProductComponent.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Link} from 'react-router-dom'
-import { useSelector } from 'react-redux';
+// import { useSelector } from 'react-redux';
 
 const ProductComponent = ({product}) => {
 //      const products = useSelector((state) => state.allProducts.products);
@@ -8,24 +8,23 @@ const ProductComponent = ({product}) => {
 //   const renderList = products.map((product) => {
     const { id, title, image, price, category } = product;
     return (
-        <div class="container" key={id}>
+        <div class="container" >
           <Link to={`/product/${id}`}  style={{textDecoration:'none', color:"black"}}>
 
              <div class="col">
-                <div class="card h-100" style={{ width: '18rem', padding:"22px" }}>
-            <img src={image} class="card-img-top" alt={title} />
+                <div class="card " style={{height:"33rem", width: '18rem', padding:"20px" }}>
+            <img src={image} class="card-img-top" style={{ height: "150px" }} alt={title} />
             <div class="card-body">
               <h5 class="card-title">{title}</h5>
               <p class="card-text">Price:  {price}$</p>
               <p class="card-text">Cetegory:  {category}</p>
               <h6  class="card-text">Product id:  {id}</h6>
-              {/* <a href="#" class="btn btn-primary">Go somewhere</a> */}
-            </div>
+               <a href="#" class="btn btn-primary">Go somewhere</a>
+             </div>
           </div>
           </div>
 
-
-          </Link>
+           </Link>
 
         </div >
 
