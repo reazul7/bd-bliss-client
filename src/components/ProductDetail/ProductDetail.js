ead7b0a2b43bc0e31b7055a98af4dda452102c9a
reazul7 <islamreazul7777@gmail.com>
2021-06-01 04:20:14 +0600

    single card style modify

--- src/components/ProductDetail/ProductDetail.js
+++ src/components/ProductDetail/ProductDetail.js
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
+import Footer from "../Shared/Footer/Footer";
 // import { selectedProduct, removeSelectedProduct } from './redux/actions/productsActions';
 import {
   selectedProduct,
@@ -31,26 +32,48 @@ const ProductDetail = () => {
     };
   }, [id]);
   return (
-    <div class="container">
-      <h2>product details</h2>
+    <div class="container pt-5 text-center">
+      <h2>Product Details</h2>
       {Object.keys(product).length === 0 ? (
-        <div>...Loading</div>
+        <div className="text-center">
+          <button class="btn btn-primary" type="button" disabled>
+            <span
+              class="spinner-grow spinner-grow-sm"
+              role="status"
+              aria-hidden="true"
+            ></span>
+            <span class="visually-hidden">Loading...</span>
+          </button>
+          <button class="btn btn-primary" type="button" disabled>
+            <span
+              class="spinner-grow spinner-grow-sm"
+              role="status"
+              aria-hidden="true"
+            ></span>
+            Loading...
+          </button>
+        </div>
       ) : (
-        <div>
-          <div class="card mb-3" style={{ maxWidth: "6000px" }}>
+        <div className="text-center" style={{margin: "auto 0"}}>
+          <div class="card mb-3">
             <div class="row g-0">
-              <div class="col-md-4">
-                <img className="img-fluid p-5" src={imageURL} alt="..." />
+              <div class="col-md-6">
+                <img className="img-fluid p-5 w-100" src={imageURL} alt="..." />
               </div>
-              <div class="col-md-8">
+              <div class="col-md-6">
                 <div className="pt-5 mt-5">
                   <div class="card-body p-5">
-                    <p class="card-title">{title}</p>
-                    <p class="card-text">{price}</p>
+                    <p class="card-title" style={{fontSize: "30px"}}>{title}</p>
+                    <p class="card-text" style={{color: "#ECBA78", fontSize: "20px", fontWeight: "bold"}}>{price}</p>
                     <p class="card-text">
                       <small class="text-muted">{category}</small>
                     </p>
                     <p class="card-text">{description}</p>
+                    <div className="service-policy">
+                      <p>Security policy (edit with Customer reassurance module)</p>
+                      <p> Delivery policy (edit with Customer reassurance module)</p>
+                      <p>Return policy (edit with Customer reassurance module)</p>
+                    </div>
                     <button className="btn-card">
                       <a href="https://bd-bliss.web.app/">Go to Home</a>
                     </button>
@@ -61,6 +84,8 @@ const ProductDetail = () => {
           </div>
         </div>
       )}
+      <hr />
+      <Footer/>
     </div>
   );
 };
